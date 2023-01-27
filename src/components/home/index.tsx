1e88d57847d84850c6e6588bdf783d9435e28ce7
Manu <le.marques.emanuel@gmail.com>
2023-01-27 17:42:41 +0100

    some html added

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -2,10 +2,21 @@ import './styles.css';
 
 export default function Home() {
     return (
-        <div className="container">
-        
-<h1>hello</h1>
+        <div className="home">
+
+            <div>
+                <h1>Presentation Moi</h1>
+            </div>
+            <div>
+                <h1>Skills</h1>
+            </div>
+            <div>
+                <h1>Projects</h1>
+            </div>
+            <div>
+                <h1>Contact</h1>
+            </div>
 
         </div>
     );
-    }
+}
