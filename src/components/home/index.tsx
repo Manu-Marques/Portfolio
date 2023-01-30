2d51457b23f761ee168f3b6cb1868daf31999fa8
Manu <le.marques.emanuel@gmail.com>
2023-01-30 11:20:18 +0100

    adding animation on text home

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,15 +1,17 @@
 import './styles.css';
-import Video from "../assets/particules.mp4";
+import Video from "../../../src/assets/particules.mp4";
 
 export default function Home() {
     return (
         <div className="home">
                 <video className='video' src={Video} autoPlay loop muted />
             <div className='container'>
-                <h1 className='text'>Hi, I'm <span className='letter'>Emanuel</span>and Welcome to my Portfolio</h1>
-            </div>
+                <div className="content">
+                    <h1 className='text'>Hi, I'm Emanuel and Welcome to my Portfolio</h1>
+                </div>
             <div>
                 <button>Entrer</button>
+            </div>
             </div>
         </div>
     );
