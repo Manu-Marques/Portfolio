6ccf8fdf85613cc83925554df047785baa947f1a
Manu <le.marques.emanuel@gmail.com>
2023-01-31 16:25:20 +0100

    display elements with flex

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -4,26 +4,31 @@ import Video from "../../../src/assets/espace.mp4";
 export default function Home() {
     return (
         <div className="home">
-                <div className="layout">
-                    <video className='video' src={Video} autoPlay loop muted />
-                </div>
+            <div className="layout">
+                <video className='video' src={Video} autoPlay loop muted />
+            </div>
             <div className='container'>
+                <div className="first-line">
+                    <div className='resume'>
+                        <h1 className='word'>About</h1>
+                        <h2 className='word'>Emanuel Marques Lé</h2>
+                        <h2 className='word'>Développeur Web Front-End</h2>
+                    </div>
+                    <div className='projects'>
+                        <h1 className='word'>Projects</h1>
+                    </div>
+                </div>
                 <div className="content">
                     <h1 className='text'>Bienvenue dans le Portfolio d'Emanuel</h1>
                 </div>
-                <div className='resume'>
-                    <h2 className=''>Emanuel Marques Lé</h2>
-                    <h2 className=''>Développeur Web Front-End</h2>
-                </div>
-                <div className='projects'>
-                    <h2>Projects</h2>
-                </div>
-                <div className='skills'>
-                    <h2>Skills</h2>
+                <div className="second-line">
+                    <div className='skills'>
+                        <h1 className='word'>Skills</h1>
                     </div>
-                <div className='contact'>
-                    <h2>Contact</h2>
+                    <div className='contact'>
+                        <h1 className='word' >Contact</h1>
                     </div>
+                </div>
             </div>
         </div>
     );
