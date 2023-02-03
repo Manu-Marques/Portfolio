41da12bfad55af9fd62421bff14164afe8e3f6e8
Manu <le.marques.emanuel@gmail.com>
2023-02-03 12:48:08 +0100

    added design for menu

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -8,16 +8,15 @@ export default function Home() {
                 <video className='video' src={Video} autoPlay loop muted />
             </div>
             <div className='container'>
-                    <div className='resume'>
-                        <h2 className='word'>Emanuel Marques Lé</h2>
-                        <h2 className='word'>Développeur Web Front-End</h2>
-                    </div>
-                 <div className="content">
+                <div className='resume'>
+                    <h2 className='word'>Bonjour et bienvenue dans le Portfolio d'Emanuel Marques Lé </h2>
+                </div>
+                <div className="content">
+                    <h1 className='experience'>Experience</h1>
                     <h1 className='skills'>Skills</h1>
                     <h1 className='about'>About</h1>
-                    <h1 className='contact'>Contact</h1>
                     <h1 className='projects'>Projects</h1>
-                    <h1 className='experience'>Experience</h1>
+                    <h1 className='contact'>Contact</h1>
                 </div>
             </div>
         </div>
