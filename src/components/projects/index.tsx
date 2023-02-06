ee97b7cad0ccafb66d9b0cffc2f903f853005875
Manu <le.marques.emanuel@gmail.com>
2023-02-06 18:09:56 +0100

    added display

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,16 +1,17 @@
 import './styles.css'
-
+import Artisenbois from './artisenbois';
 
 export default function Projects() {
     return (
         <div className="projects-main">
             <h1 className='projects-title'>Projects</h1>
-            <div className="projects-container">
-            </div>
-            <div>
-                <h2 className='projects-subtitle'>Artisenbois</h2>
-                <h2 className='projects-subtitle'>ToDoList</h2>
-                <h2 className='projects-subtitle'>Pokemon Finder</h2>
+            <div className="projects-display">
+                <div className='projects-navigate'>
+                    <input type="radio"></input>
+                    <label className='projects-subtitle'>Artisenbois</label>
+                </div>
+                <div className="projects-container">
+                </div>
             </div>
         </div>
     );
--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -1,7 +1,23 @@
+import "./styles.css";
+
 export default function Skills() {
     return (
-        <div className="skills">
-            <h1>Skills</h1>
-        </div>
+        <div className="main">  
+        <div className="btn-box">  
+             <button className="btn active">Tab One</button>  
+             <button className="btn"> Tab Two</button>  
+             <button className="btn">Tab Three</button>  
+        </div>  
+        <div className="contents">  
+             <h1>Heading One</h1>  
+             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod  
+             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,  
+             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo  
+             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse  
+             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non  
+             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  
+             <a href="#" className="hero-btn">Learn More</a>  
+        </div>  
+   </div>  
     );
 }
