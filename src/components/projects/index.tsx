55252284a889bf18747d24d8ef5066fb10457fe8
Manu <le.marques.emanuel@gmail.com>
2023-02-07 14:16:10 +0100

    design for display projects

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -3,16 +3,39 @@ import Artisenbois from './artisenbois';
 
 export default function Projects() {
     return (
-        <div className="projects-main">
-            <h1 className='projects-title'>Projects</h1>
-            <div className="projects-display">
-                <div className='projects-navigate'>
-                    <input type="radio"></input>
-                    <label className='projects-subtitle'>Artisenbois</label>
-                </div>
-                <div className="projects-container">
-                </div>
+        <div className="projects-main">  
+        <h1 className="projects-title">Projects</h1> 
+        <div className="projects-table">
+            <div className="btn-box">
+                 <button className="btn">ArtisenBois</button>
+                 <button className="btn">ToDoList</button>
+                 <button className="btn">Pokemon Finder</button>
+            </div>
+            <div className="contents">
+                 <h1>Heading One</h1>
+                 <p>Loreua. Ut enim ad minim veniam,
+                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                 consequat. Duis aute irure dolor infqdolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                 consequat. Duis aute irure dolor infqdolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                 consequat. Duis aute irure dolor infqdolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                 consequat. Duis aute irure dolor infqdolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                 consequat. Duis aute irure dolor infqdolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                 consequat. Duis aute irure dolor infqdte irure dolor infqdsfqsfqdsfdsdqsfsqfsfsqfdfsqfqsdfsfsqfqsfdsqfsqfsqdfsfqfqfdsqfdsqf reprehenderit in voluptate velit esse
+                 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                 <a href="#" className="hero-btn">Visiter le site</a>
             </div>
         </div>
+   </div>  
     );
 }
