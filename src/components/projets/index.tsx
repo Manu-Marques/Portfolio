4d19e4b1eff62939a09d986bb82477bbd007ba41
Manu <le.marques.emanuel@gmail.com>
2023-02-15 23:00:15 +0100

    adding an arrow as png btn back

--- src/components/projets/index.tsx
+++ src/components/projets/index.tsx
@@ -2,12 +2,16 @@ import './styles.css'
 import Artisenbois from './artisenbois';
 import Todolist from './todolist';
 import PokemonFinder from './pokemonFinder';
+import Arrow from '../../assets/arrow.png';
 
 export default function Projets() {
     return (
         <div className="projets-main">  
-        <h1 className="projets-title">Projets</h1> 
-        <button className="btn-back">Retour</button>
+        <h1 className="projets-title">Projets</h1>
+        <div className="projets-btn-arrow">
+            <img className="projets-arrow" src={Arrow} alt="arrow"/>
+            <button className="btn-back">Retour</button>
+        </div>
         <div className="projets-table">
             <div className="projets-btn">
                  <button className="btn">ArtisenBois</button>
