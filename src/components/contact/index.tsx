272864c527f4f3364393b2134e5c7d0abf957de2
Manu <le.marques.emanuel@gmail.com>
2023-02-17 17:31:58 +0100

    starting design for contact

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,7 +1,17 @@
+import './styles.css';
+
 export default function Contact() {
     return (
-        <div className="contact">
-            <h1>Contact</h1>
+        <div className="contact-main">
+            <h1 className='contact-title'>Contact</h1>
+            <div className="contact-btn">
+                <button className="contact-btn-back">Retour</button>
+            </div>
+            <div className="contact-table">
+                <div className="contact-container">
+                    
+                </div>
+            </div>
         </div>
     );
 }
