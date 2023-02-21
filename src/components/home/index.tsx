fa12ab2533524604a2acf97f3cab97b09c1c6def
Manu <le.marques.emanuel@gmail.com>
2023-02-21 18:19:06 +0100

    responsive for home

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -5,17 +5,19 @@ import photo from '../../../src/assets/photo.jpg';
 export default function Home() {
     return (
         <div className="home">
-            <div className='container'>
-                <div className='resume'>
-                    <h2 className='presentation'>Bonjour, je m'appelle <span className="presentation-word">Emanuel</span></h2>
-                    <h2 className='presentation'>Je suis <span className="presentation-word">Développeur Front-End</span></h2>
-                    <img className='presentation-img' src={photo} alt="moi" />
-                </div>
-                <div className="content">
-                    <Link to="/experience" className='experience'>Expériences</Link>
-                    <Link to="/competences" className='skills'>Compétences</Link>
-                    <Link to="/projets" className='projets'>Projets</Link>
-                    <Link to="/formations" className='formations'>Formations</Link>
+            <div className='home-container'>
+                <div className="home-row">
+                    <div className='home-resume'>
+                        <h2 className='home-presentation'>Bonjour, je m'appelle <span className="presentation-word">Emanuel</span></h2>
+                        <h2 className='home-presentation'>Je suis <span className="presentation-word">Développeur Front-End</span></h2>
+                        <img className='home-img' src={photo} alt="moi" />
+                    </div>
+                    <div className="home-content">
+                        <Link to="/experience" className='home-experience'>Expériences</Link>
+                        <Link to="/competences" className='home-skills'>Compétences</Link>
+                        <Link to="/projets" className='home-projets'>Projets</Link>
+                        <Link to="/formations" className='home-formations'>Formations</Link>
+                    </div>
                 </div>
             </div>
         </div>
