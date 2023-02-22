658d99793cc4e371e68830bcd055c0d5a09af8d1
Manu <le.marques.emanuel@gmail.com>
2023-02-22 17:17:56 +0100

    added more design for home responsive

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -6,23 +6,31 @@ export default function Contact() {
         <div className="contact">
             <div className="contact-container">
                 <div className="contact-icons">
-                    <div className="contact-envelope">
-                        <i className="fa fa-envelope fa-2x"></i>
+                    <div className="contact-left">
+                        <div className="contact-top">
+                            <div className="contact-envelope">
+                                <i className="fa fa-envelope fa-2x"></i>
+                            </div>
+                            <p className='contact-email'>le.marques.emanuel@gmail.com</p>
+                        </div>
+                        <div className="contact-bottom">
+                            <div className="contact-phone">
+                                <i className="fa fa-phone fa-2x"></i>
+                            </div>
+                            <p className='contact-number'>+33 665 24 08 60</p>
+                        </div>
                     </div>
-                        <p className='contact-email'>le.marques.emanuel@gmail.com</p>
-                    <div className="contact-phone">
-                        <i className="fa fa-phone fa-2x"></i>
-                    </div>
-                        <p className='contact-number'>+33 665 24 08 60</p>
-                    <div className="contact-git">
-                        <Link to="https://github.com/Manu-Marques" target="_blank">
-                            <i className="fa fa-github fa-2x"></i>
-                        </Link>
-                    </div>
-                    <div className="contact-linkdin">
-                        <Link to="https://www.linkedin.com/in/emanuel-marques-le/" target="_blank">
-                        <i className="fa fa-linkedin fa-2x"></i>
-                        </Link>
+                    <div className="contact-right">
+                        <div className="contact-git">
+                            <Link to="https://github.com/Manu-Marques" target="_blank">
+                                <i className="fa fa-github fa-2x"></i>
+                            </Link>
+                        </div>
+                        <div className="contact-linkdin">
+                            <Link to="https://www.linkedin.com/in/emanuel-marques-le/" target="_blank">
+                                <i className="fa fa-linkedin fa-2x"></i>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
