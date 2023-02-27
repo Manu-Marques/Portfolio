5b317e802faaa467fd1d3298e230131c4825d5ad
Manu <le.marques.emanuel@gmail.com>
2023-02-27 17:10:49 +0100

    new contact with responsive

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -21,7 +21,7 @@ export default function App() {
         <Route path="/formations" element={<Formations />} />
       </Routes>
       <Background />
-      <Contact />
+       <Contact /> 
     </div>
   );
 }
