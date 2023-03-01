0d83efbe56e0dc154acef7872de8a8d426b91bed
Manu <le.marques.emanuel@gmail.com>
2023-03-01 22:11:35 +0100

    new repo

---
+++ src/components/experience/index.tsx
@@ -0,0 +1,76 @@
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+export default function Experience() {
+    return (
+        <div className="experience-main">
+            <h1 className='experience-title'>Expériences</h1>
+            <div className="experience-btn">
+            <Link to="/">
+                <button className="experience-btn-back">Retour</button>
+            </Link>
+            </div>
+            <div className="experience-table">
+                <div className="experience-container">
+                    <div className="experience-left">
+                        <div className='experience-oclock'>
+                            <h1 className='experience-subtitle'>Développeur FullStack</h1>
+                            <h1 className='experience-oclock-subtitle'>O'Clock - Projet fin d'études</h1>
+                            <ul className='experience-oclock-list'>
+                                <li className='experience-item'>- Développement en Front et Back</li>
+                                <li className='experience-item'>- Utilisation de la méthode Scrum</li>
+                                <li className='experience-item'>- Rédaction des documents personnelles</li>
+                                <li className='experience-item'>(Journal de bord, document de veille)</li>
+                                <li className='experience-item'>- Utilisation de la méthode Kanban </li>
+                            </ul>
+                            <div className="experience-row">
+                                <p className='experience-year'>Avril 2022 à Mai 2022</p>
+                            </div>
+                        </div>
+                        <div className="experience-stavo">
+                            <h1 className='experience-subtitle'>Chauffeur de Bus</h1>
+                            <h1 className='experience-stavo-subtitle'>Stavo - Plaisir</h1>
+                            <ul className='experience-stavo-list'>
+                                <li className='experience-item'>- Transport de personnes</li>
+                                <li className='experience-item'>- Respect des horaires</li>
+                                <li className='experience-item'>- Respect des règles de sécurité et de conduite</li>
+                                <li className='experience-item'>- Gestion de la relation client</li>
+                            </ul>
+                            <div className="experience-row">
+                                <p className='experience-year'>Décembre 2013 à Décembre 2021</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="experience-right">
+                        <div className="experience-hotel">
+                            <h1 className='experience-subtitle'>Réceptionniste</h1>
+                            <h1 className='experience-hotel-subtitle'>Hotel F1- Plaisir</h1>
+                            <ul className='experience-hotel-list'>
+                                <li className='experience-item'>- Accueil des clients</li>
+                                <li className='experience-item'>- Gestion des réservations et des chambres</li>
+                                <li className='experience-item'>- Gestion des horaires de départ</li>
+                                <li className='experience-item'>- Gestion des stocks</li>
+                            </ul>
+                            <div className="experience-row">
+                                <p className='experience-year'>Novembre 2010 à Novembre 2012</p>
+                            </div>
+                        </div>
+                        <div className="experience-restaurant">
+                            <h1 className='experience-subtitle'>Équipier Polyvalent</h1>
+                            <h1 className='experience-restaurant-subtitle'>Quick - Coignières</h1>
+                            <ul className='experience-restaurant-list'>
+                                <li className='experience-item'>- Travail en équipe</li>
+                                <li className='experience-item'>- Gestion de la relation client</li>
+                                <li className='experience-item'>- Préparation des commandes</li>
+                                <li className='experience-item'>- Gestion des stocks</li>
+                            </ul>
+                            <div className="experience-row">
+                                <p className='experience-year'>Novembre 2009 à Octobre 2010</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/projets/pokemonFinder/index.tsx
@@ -0,0 +1,35 @@
+import logo from '../../../assets/pokeball.jpg';
+import './styles.css'
+
+export default function PokemonFinder() {
+    return (
+        <div className="pokemon">
+            <h1 className='pokemon-title'>Pokemon Finder</h1>
+            <div className="pokemon-container">
+                <div className="pokemon-container-img">
+                    <img className='pokemon-img' src={logo}>
+                    </img>
+                </div>
+                <div className="pokemon-info">
+                    <h2 className="pokemon-subtitle">Résumé du projet</h2>
+                    <p className="pokemon-text">Ce site web a été réalisé dans le cadre d'un projet perso. Le but était de créer un site avec la consommation et manipulation d'une API disponible en ligne, ainsi que des tests unitaires pour les functions.</p>
+                    <div className="pokemon-description">
+                        <h2 className="pokemon-subtitle">Outils de développement utilisés</h2>
+                        <ul className="pokemon-list">
+                            <li className="pokemon-item">Design <span className="pokemon-word">HTML et Tailwind</span></li>
+                            <li className="pokemon-item">Langage <span className="pokemon-word">JavaScript</span></li>
+                            <li className="pokemon-item">Framework <span className="pokemon-word">React</span></li>
+                            <li className="pokemon-item">API <span className="pokemon-word">Pokebuildapi</span></li>
+                            <li className="pokemon-item">Tests unitaires <span className="pokemon-word">Jest</span></li>
+                            <li className="pokemon-item">Deploiement avec <span className="pokemon-word">Vercel</span></li>
+                            <li className="pokemon-item">Responsive avec <span className="pokemon-word">Media Query</span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="pokemon-visite">
+                <a href="https://pokemon-finders.vercel.app/" target="_blank" className="pokemon-link">Visiter le site</a>
+            </div>
+        </div>
+    );
+}
